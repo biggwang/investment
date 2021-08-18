69c0a47badb90246d41b4ac9a54bf656826c0098
류윤광 <ghmryg@gmail.com>
2021-08-19 01:05:52 +0900

    레디스 동시 카운트 로직 테스트

--- src/main/java/com/kakaopay/ryuyungwang/investment/InvestmentApplication.java
+++ src/main/java/com/kakaopay/ryuyungwang/investment/InvestmentApplication.java
@@ -2,7 +2,9 @@ package com.kakaopay.ryuyungwang.investment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class InvestmentApplication {
 
