757f8199d240de42d3b92a077977acbec7f1cf64
류윤광/회원채널개발팀 <yungwang.ryu@musinsa.com>
2021-08-22 22:03:24 +0900

    docker compose helath check 기능 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,11 @@
-insert into product (title, total_investing_amount, status, started_at, finished_at) values ('가즈아', 2000, 'RECRUITMENT', '2021-08-01 00:00:00', '2021-08-24 00:00:00');
+# 모집기간 지난 상품
+insert into product (title, total_investing_amount, current_investing_amount, total_invester, status, started_at, finished_at) values ('개인신용 분산투자(선순위) 1', 1000000, 1000000, 10, 'FINISHED', '2019-03-01 00:00:00', '2019-12-31 23:59:59');
+insert into product (title, total_investing_amount, current_investing_amount, total_invester, status, started_at, finished_at) values ('아파트 담보(서울시 서대문구) 2', 3000000, 3000000, 23, 'FINISHED', '2019-03-01 00:00:00', '2019-12-31 23:59:59');
+insert into product (title, total_investing_amount, current_investing_amount, total_invester, status, started_at, finished_at) values ('아파트 담보(서울시 도봉구) 3', 5000000, 5000000, 456, 'FINISHED', '2019-03-01 00:00:00', '2019-12-31 23:59:59');
+
+# 모집기간 상품
+insert into product (title, total_investing_amount, status, started_at, finished_at) values ('아파트 담보(서울시 강서구) 4', 7000000, 'FINISHED', '2021-04-01 00:00:00', '2021-09-15 23:59:59');
+insert into product (title, total_investing_amount, status, started_at, finished_at) values ('아파트 담보(서울시 서초구) 5', 20000000, 'FINISHED', '2021-05-01 00:00:00', '2021-10-23 23:59:59');
+insert into product (title, total_investing_amount, status, started_at, finished_at) values ('아파트 담보(서울시 은평구) 6', 50000000, 'FINISHED', '2021-06-01 00:00:00', '2021-11-05 23:59:59');
+insert into product (title, total_investing_amount, status, started_at, finished_at) values ('아파트 담보(서울시 강북구) 7', 80000000, 'FINISHED', '2021-07-01 00:00:00', '2021-12-31 23:59:59');
+insert into product (title, total_investing_amount, status, started_at, finished_at) values ('아파트 담보(서울시 강남구) 8', 90000000, 'FINISHED', '2021-08-01 00:00:00', '2021-12-31 23:59:59');
